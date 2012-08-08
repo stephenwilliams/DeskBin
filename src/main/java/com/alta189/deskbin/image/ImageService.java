7e460ac13740cc3f2f53efc26368615bc0350ff5
alta189 <me@alta189.com>
2012-08-08 00:23:35 -0400

    Update Image Fixed ImgurService

--- src/main/java/com/alta189/deskbin/image/ImageService.java
+++ src/main/java/com/alta189/deskbin/image/ImageService.java
@@ -3,6 +3,8 @@ package com.alta189.deskbin.image;
 import com.alta189.deskbin.Service;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Collection;
 import java.util.List;
 
 public interface ImageService extends Service {
@@ -10,4 +12,9 @@ public interface ImageService extends Service {
 	public String upload(BufferedImage image) throws ImageServiceException;
 
 	public String upload(List<BufferedImage> image) throws ImageServiceException;
+
+	public String upload(File image) throws ImageServiceException;
+
+	public String upload(Collection<File> images) throws ImageServiceException;
+
 }
--- src/main/java/com/alta189/deskbin/image/ImgurService.java
+++ src/main/java/com/alta189/deskbin/image/ImgurService.java
@@ -1,21 +1,22 @@
 package com.alta189.deskbin.image;
 
+import com.alta189.deskbin.util.ScreenshotUtil;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.apache.commons.codec.binary.Base64;
+import org.json.JSONObject;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
+import java.nio.charset.Charset;
+import java.util.Collection;
 import java.util.List;
 
 public class ImgurService implements ImageService {
@@ -36,29 +37,59 @@ public class ImgurService implements ImageService {
 	@Override
 	public String upload(BufferedImage image) throws ImageServiceException {
 		try {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(image, "png", baos);
-
-			HttpPost post = new HttpPost(UPLOAD_URL);
-
-			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-			nvps.add(new BasicNameValuePair("key", apikey));
-			nvps.add(new BasicNameValuePair("image", URLEncoder.encode(Base64.encodeBase64String(baos.toByteArray()), "UTF-8")));
-			nvps.add(new BasicNameValuePair("type", "base64"));
-
-			post.setEntity(new UrlEncodedFormEntity(nvps));
-
-			HttpResponse response = client.execute(post);
-			System.out.println(EntityUtils.toString(response.getEntity()));
-		} catch (IOException e) {
-			e.printStackTrace();
+			File file = File.createTempFile("screenshot-", ".png");
+			ImageIO.write(image, "png", file);
+			return upload(file);
+		} catch (ImageServiceException e) {
+		    throw e;
+		}catch (IOException e) {
+			throw new ImageServiceException(e);
+		} catch (Exception e) {
+			throw new ImageServiceException(e);
 		}
-
-		return null;
 	}
 
 	@Override
-	public String upload(List<BufferedImage> image) throws ImageServiceException {
-		return null;
+	public String upload(List<BufferedImage> images) throws ImageServiceException {
+		StringBuilder result = new StringBuilder();
+		for (BufferedImage image : images) {
+			result.append(upload(image))
+					.append(" ");
+		}
+		return result.toString();
+	}
+
+	@Override
+	public String upload(File image) throws ImageServiceException {
+		try {
+			HttpPost post = new HttpPost(UPLOAD_URL);
+
+			MultipartEntity entity = new MultipartEntity();
+			entity.addPart("key", new StringBody(apikey, Charset.forName("UTF-8")));
+			entity.addPart("type", new StringBody("file", Charset.forName("UTF-8")));
+			FileBody fileBody = new FileBody(image);
+			entity.addPart("image", fileBody);
+			post.setEntity(entity);
+
+			HttpResponse response = client.execute(post);
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new ImageServiceException("Error accessing imgur. Status code: " + response.getStatusLine().getStatusCode());
+			}
+			String json = EntityUtils.toString(response.getEntity());
+			JSONObject jsonObject = new JSONObject(json).getJSONObject("upload").getJSONObject("links");
+			return jsonObject.getString("imgur_page");
+		} catch (Exception e) {
+			throw new ImageServiceException(e);
+		}
+	}
+
+	@Override
+	public String upload(Collection<File> images) throws ImageServiceException {
+		StringBuilder result = new StringBuilder();
+		for (File image : images) {
+			result.append(upload(image))
+					.append(" ");
+		}
+		return result.toString();
 	}
 }
