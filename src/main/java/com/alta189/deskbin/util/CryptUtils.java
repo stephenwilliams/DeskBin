32d5717e8324fcd1bbcf4af4413840c59e9c8592
alta189 <me@alta189.com>
2012-08-16 11:22:57 -0400

    LGPL

--- src/main/java/com/alta189/deskbin/util/CryptUtils.java
+++ src/main/java/com/alta189/deskbin/util/CryptUtils.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of DeskBin.
+ *
+ * Copyright (c) ${project.inceptionYear}-2012, alta189 <http://github.com/alta189/DeskBin/>
+ * DeskBin is licensed under the GNU Lesser General Public License.
+ *
+ * DeskBin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DeskBin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.alta189.deskbin.util;
 
 import java.io.File;
